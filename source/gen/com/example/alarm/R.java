9be386e41be5fbd2fcd952f7d876df0ddc90cc4e
junweizhang23 <junweizhang23@gmail.com>
2014-01-24 23:13:48 -0500

    add and sub

--- source/gen/com/example/alarm/R.java
+++ source/gen/com/example/alarm/R.java
@@ -25,7 +25,11 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090000;
+        public static final int Jason=0x7f090000;
+        public static final int Sum=0x7f090003;
+        public static final int action_settings=0x7f090004;
+        public static final int bAdd=0x7f090002;
+        public static final int bSub=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -37,6 +41,10 @@ public final class R {
         public static final int background=0x7f040000;
     }
     public static final class string {
+        public static final int Add=0x7f060004;
+        public static final int JasonColor=0x7f060003;
+        public static final int Substract=0x7f060005;
+        public static final int Sum=0x7f060006;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
--- source/src/com/example/alarm/MainActivity.java
+++ source/src/com/example/alarm/MainActivity.java
@@ -3,13 +3,48 @@ package com.example.alarm;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	int counter;
+	Button add;
+	Button sub;
+	TextView display;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		counter = 0;
+
+		add = (Button) findViewById(R.id.bAdd);
+		sub = (Button) findViewById(R.id.bSub);
+		display = (TextView) findViewById(R.id.Sum);
+
+		add.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				counter = counter + 1;
+				display.setText("You total is : " + counter);
+			}
+		});
+
+		sub.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				counter = counter - 1;
+				display.setText("You total is : " + counter);
+			}
+		});
+
 	}
 
 	@Override
